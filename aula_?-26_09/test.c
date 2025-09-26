c78a3ad9af4239694600e09d78f178a46840cf20
vitor <vitor.amancio@estudante.ifms.edu.br>
2025-09-26 12:22:44 -0400

    new file:   aula_?-26_09/a.out

---
+++ aula_?-26_09/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void trocar(int *x,int *y){
+  int temp = *x;
+  *x = *y;
+  *y = temp;   
+}
+
+void trocarp(int **x,int **y){
+  int *temp = *x;
+  *x = *y;
+  *y = temp;   
+}
+
+int main(){
+  int c,d;
+  int *a=&c, *b=&d;
+  *a = 10;
+  *b = 5000;
+  trocar(a,b);
+  printf("a = %d \nb = %d\n",*a,*b);
+}
